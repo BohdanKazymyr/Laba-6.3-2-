6b42264fa16ef20c8893e60c3cc48dbf9d3ab77e
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-11-10 17:26:28 +0200

    f

---
+++ Laba 6.3(2)/Laba 6.3(2).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, int Low, int High, int i)
+{
+
+	if (i < size)
+	{
+		a[i] = Low + rand() % (High - Low + 1);
+		return Create(a, size, Low, High, i + 1);
+	}
+}
+void Print(int* a, const int size, int i)
+{
+	if (i <= size)
+	{
+		cout << setw(4) << a[i];
+		return Print(a, size, i + 1);
+		cout << endl;
+	}
+}
+int Min(int* a, const int size, int i, int min)
+{
+	if (a[i] < min)
+		min = a[i];
+	if (i < size - 1)
+		return Min(a, size, i + 1, min);
+	else
+		return min;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 21;
+	int i = 0;
+	int a[n];
+	int Low = 15;
+	int High = 85;
+	Create(a, n, Low, High, i);
+	Print(a, n, i);
+	cout << "                            Min = " << Min(a, n, 1, a[0]) << endl;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 6.3(2)/Laba 6.3(2).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 21;
+			int i = 0;
+			int a[n];
+			int Low = 15;
+			int High = 85;
+			Create(a, n, Low, High, i);
+			Print(a, n, i);
+			Min(a, n, 1, a[0]);
+		}
+	};
+}
